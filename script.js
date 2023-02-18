96aa84486df57ec1555e286e535472aaac86931b
Nitaki Nagachiro <64524466+Nitaki-dev@users.noreply.github.com>
2023-02-18 19:23:56 +0100

    o

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
-// Get the form element
-const form = document.querySelector('form');
-
-// Add a submit event listener to the form
-form.addEventListener('submit', (event) => {
-  // Prevent the form from submitting
-  event.preventDefault();
-
-  // Get the form data
-  const formData = new FormData(form);
-
-  // Send the form data to the server
-  fetch('/send', {
-    method: 'POST',
-    body: formData
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      // Display a success or error message
-      alert(data.message);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-});
+function validateForm() {
+    // Get the form elements
+    const name = document.forms["contactForm"]["name"];
+    const email = document.forms["contactForm"]["email"];
+    const message = document.forms["contactForm"]["message"];
+  
+    // Validate the form elements
+    if (name.value == "") {
+      alert("Name must be filled out");
+      name.focus();
+      return false;
+    }
+    if (email.value == "") {
+      alert("Email must be filled out");
+      email.focus();
+      return false;
+    }
+    if (message.value == "") {
+      alert("Message must be filled out");
+      message.focus();
+      return false;
+    }
+  
+    // If all form elements are valid, return true
+    return true;
+  }
+  
